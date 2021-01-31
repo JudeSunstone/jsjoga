b290e12b3e86f178172e273f0d204978fad5e929
Regina Grigorjeva <regina.grigorjeva@gmail.com>
2021-01-31 21:25:21 +0200

    yay

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//сперва надо обработчик событий, чтобы не мешали разные другие проблемы
+window.addEventListener("DOMcontentLoaded", function () {
+    'use strict';
+    let tab = document.querySelectorAll(".info-header-tab"),
+        info = document.querySelector(".info-header"),
+        tabContent = document.querySelectorAll(".info-tabcontent");
+
+
+});
