1459589a3c74cc3d37401ace8c7faefa4f3c0630
Regina Grigorjeva <regina.grigorjeva@gmail.com>
2021-02-02 11:48:40 +0200

    is js connected

--- js/script.js
+++ js/script.js
@@ -1,9 +1,34 @@
 //сперва надо обработчик событий, чтобы не мешали разные другие проблемы
-window.addEventListener("DOMcontentLoaded", function () {
+window.addEventListener('DOMcontentLoaded', function () {
     'use strict';
     let tab = document.querySelectorAll(".info-header-tab"),
         info = document.querySelector(".info-header"),
         tabContent = document.querySelectorAll(".info-tabcontent");
 
+    function hideTabContent (a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove("show");
+            tabContent[i].classList.add("hide");
+        }
+    }
+    hideTabContent(1); // через единциы скрываются все табы кроме именно первого
 
+    function showTabContent (b) {
+        if (tabContent(b).classList.contains("hide"))  {
+            tabContent[b].classList.remove("hide");
+            tabContent[b].classList.add("show");
+        } 
+    }
+    info.addEventListener('click', function(event) {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for(let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0); // hide all tabs
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
 });
