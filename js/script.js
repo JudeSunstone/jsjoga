422241feed0111da789e7aef887a0e5d37fcabf5
Regina Grigorjeva <regina.grigorjeva@gmail.com>
2021-02-22 17:19:10 +0200

    class-new-constructor

--- js/script.js
+++ js/script.js
@@ -74,3 +74,55 @@ window.addEventListener('DOMcontentLoaded', function () {
 
 
 });
+
+// конструктор и класс
+
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    this.hello = function() {
+        console.log("Пользователь " + this.name + " ушел");
+    };
+}
+
+    User.prototype.exit = function(name) {
+        console.log("Пользователь " + this.name + " ушел");
+    }
+    
+
+    let ivan = new User('Iven', 25),
+        alex = new User('Alex', 20);
+
+    console.log(ivan);
+    console.log(alex);
+
+    ivan.exit();
+
+    // sintaxis sugar
+
+    class User {
+        constructor(name, id) {
+            this.name = name;
+            this.id = id;
+            this.human = true;
+        }
+        hello() {
+            console.log("Hello!   ${this.name}");
+        }
+
+        exit() {
+            console.log("Пользователь  ${this.name} ушел");
+        }
+    
+
+    }   
+    
+    let ivan = new User('Iven', 25),
+            alex = new User('Alex', 20);
+    
+        console.log(ivan);
+        console.log(alex);
+    
+        ivan.hello();
+
