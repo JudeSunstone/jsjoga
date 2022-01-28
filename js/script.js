84113fa9f8ead8dd3bdadd2dcf34d013635af8dc
Regina Grigorjeva <30758656+JudeSunstone@users.noreply.github.com>
2022-01-28 15:12:55 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11820,4 +11820,113 @@ export default App;
 https://codesandbox.io/s/huki-primer-dlya-lekcii-useeffect-77cb9?file=/src/App.js:0-1184
 
 
+переход на странички
 
+import React, { PureComponent } from "react";
+import {
+  BrowserRouter,
+  Route,
+  Link,
+  useParams,
+  useHistory,
+  useLocation
+} from "react-router-dom";
+
+function CatId() {
+  const params = useParams();
+  const history = useHistory();
+  const location = useLocation();
+  console.log(params, history, location);
+  return <span>Cat {params.id}</span>;
+}
+
+function Cat() {
+  return (
+    <article>
+      <img alt="cat" src="/cat.png" />
+    </article>
+  );
+}
+
+function Plant() {
+  return (
+    <article>
+      <img alt="cat" src="/plant.jpeg" />
+    </article>
+  );
+}
+
+class PlantId extends PureComponent {
+  render() {
+    console.log(this.props);
+    return <span>Plant {this.props.match.params.id}</span>;
+  }
+}
+
+function Person() {
+  return (
+    <article>
+      <img alt="cat" src="/person.png" width="300" />
+    </article>
+  );
+}
+
+function Nav() {
+  return (
+    <header>
+      <nav className="nav">
+        <Link className="nav-link" to="/animals">
+          Animals
+        </Link>
+        <Link className="nav-link" to="/plants">
+          Plants
+        </Link>
+        <Link className="nav-link" to="/people">
+          People
+        </Link>
+      </nav>
+      <nav>
+        <Link className="nav-link" to="/animals/123">
+          id: 123
+        </Link>
+        <Link className="nav-link" to="/plants/456">
+          id: 456
+        </Link>
+      </nav>
+    </header>
+  );
+}
+
+function Page() {
+  return (
+    <main>
+      <Route path="/animals">
+        <Cat />
+      </Route>
+      <Route path="/animals/:id">
+        <CatId />
+      </Route>
+      <Route path="/plants">
+        <Plant />
+      </Route>
+      <Route path="/plants/:id" component={PlantId} />
+      <Route path="/people">
+        <Person />
+      </Route>
+    </main>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Page />
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
+
+https://codesandbox.io/s/routing-react-router-dom-uvdmu?file=/src/App.js:0-1885
